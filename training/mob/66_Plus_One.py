f81bcc86014801fe69d7b2be7366fb06065d72b6
akarmain <akarmain@MacBookAirM4akrmain.local>
2026-01-02 15:47:47 +0300

    mob

--- training/mob/66_Plus_One.py
+++ training/mob/66_Plus_One.py
@@ -1,3 +1,14 @@
-# class Solution:
-#     def plusOne(self, digits: List[int]) -> List[int]:
-#         return [1, 2, 3]
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                break
+        else:
+            return [1] + ([0] * len(digits))
+        return digits
+
+
+if __name__ == "__main__":
+    ans = Solution()
+    print(ans.plusOne([9, 9, 0, 9]))
