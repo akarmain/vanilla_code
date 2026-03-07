ddd439177a75c3b48b15707f3343faf57ced7472
akarmain <dev@akarmain.ru>
2026-03-07 15:01:36 +0300

    Системы искусственного интеллекта и большие данные Практическая работа №1

--- training/mob/fun-with-arrays/Find Numbers with Even Number of Digits.py
+++ training/mob/fun-with-arrays/Find Numbers with Even Number of Digits.py
@@ -2,11 +2,10 @@ class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         ans = 0
         for i in nums:
-            if len(str(i)) % 2 == 0:
+            if len(str(i)) %2== 0:
                 ans += 1
-        return ans
-
+        return ans 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.findNumbers([1, 11, 1, 10, 11, 1, 1, 0]))
+    print(s.findNumbers([1, 11, 1, 10, 11, 1, 0, 0]))
